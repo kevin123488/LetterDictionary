e970070d4aa7a580e29bc46643fda3fb8976c8f6
박정현 <kevin123488@naver.com>
2022-09-05 15:39:51 +0900

    feat : axios 세팅
    axios세팅했어요
    
    
    Former-commit-id: 5daf0903878d0611895bcd9f16bb2d9605e2e051

---
+++ front/src/api/index.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e6d2fdd058d98a31812953d8dc6e66b19765a78da9f2037ac789d3d3e13faaae
+size 292
---
+++ front/src/config/index.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:232ed5a5c235d68c2bf53c3d61a850bd56e4840dbc7d6ded283c0c6c2e6a95e9
+size 104
