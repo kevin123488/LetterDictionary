f20d734336047838b2b840de3b799781dce809e2
jimin6206 <jimin6206@gmail.com>
2022-09-07 16:18:45 +0900

    feat : 기부 API user,foundation 추가
    기능 추가
    
    
    Former-commit-id: c641eaae826c2f403284b4572879689a8033f24c

--- back/src/main/java/com/ssafy/api/controller/BoardController.java
+++ back/src/main/java/com/ssafy/api/controller/BoardController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a5723a78b191d9ac980724496eb1df7e76205531e5c20dfda1891d4d80e12036
-size 4852
+oid sha256:cedc4d3c7eec93d009b601965f920aeafeb539c6d391f43a7fc9718adb5eff2c
+size 4886
