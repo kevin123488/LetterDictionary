af3174167e500859fb3b1d3f92b204390f1ca415
박정현 <kevin123488@naver.com>
2022-09-07 16:50:34 +0900

    feat : 메인페이지 화면 구성, 메인페이지 axios 추가
    완료했습니다
    
    
    Former-commit-id: e9c40aa2cf59bfbcf343750f1690bfe3098913a5

--- front/src/views/MainView.vue
+++ front/src/views/MainView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7a74520245ddddc3fca2df159d3f1caaa4ba2fb5bd141206b17b025bfaee5714
-size 3547
+oid sha256:a3bb7cc3b060e166573e2e5a49c008b40e60b32a80aa819fedf9e8dc7bda5b83
+size 8992
