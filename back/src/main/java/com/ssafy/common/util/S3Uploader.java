eb046b21b159e51f7f1dcc2c62758485eb8cfe98
민경대 <rudwl1005@naver.com>
2022-09-07 11:03:32 +0900

    feat : 엽서 API 개발
    게빌중 컴퓨터 이동으로 인한 커밋
    
    
    Former-commit-id: 434c1cb84dc5aa29063ef5be2b81bab9682d08a8

---
+++ back/src/main/java/com/ssafy/common/util/S3Uploader.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:0e69bcaf8f57bda37b58b19b084ad7598b0332f8b51da88ed023914f3a131f86
+size 2549
---
+++ back/src/main/java/com/ssafy/db/repository/PostcardRepository.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c412f9af21b530747d8eec9b9564c940ce4dad0a2e2ab97c9c274d0e4332cfa7
+size 321
