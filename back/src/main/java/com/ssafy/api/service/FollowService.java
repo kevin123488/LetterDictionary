7b3d5552a0f33df8f4920813447b1170346a7f20
박정현 <kevin123488@naver.com>
2022-09-14 14:15:35 +0900

    feat : 임시커밋
    임시커밋입니다
    
    
    Former-commit-id: ddf10b2c73cc1518a4827cc4414d5ff790441838

--- back/src/main/java/com/ssafy/api/service/FollowService.java
+++ back/src/main/java/com/ssafy/api/service/FollowService.java
@@ -1,3 +1,15 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:51ef04fd19e7a8121f1d05cab3a5ebe2a3231d151499955edeb4addb10ba65de
-size 468
+package com.ssafy.api.service;
+
+import com.ssafy.db.entity.Follow;
+
+import java.io.IOException;
+import java.util.List;
+
+/**
+ *	팔로우 관련 비즈니스 로직 처리를 위한 서비스 인터페이스 정의.
+ */
+public interface FollowService {
+    String saveFollow(String myId, String followId) throws IOException;
+    boolean deleteFollow(String myId, String followId) throws IOException;
+    List<Follow> selectFollowList(String myId) throws IOException;
+}
--- front/src/views/DonationView.vue
+++ front/src/views/DonationView.vue
@@ -1,14 +1,3 @@
-<template>
-    <div>Donation</div>
-  </template>
-  
-  <script>
-  export default {
-    name: "DonationView",
-  
-  }
-  </script>
-  
-  <style>
-  
-  </style>
+version https://git-lfs.github.com/spec/v1
+oid sha256:ffe0dfadddfa95df97431406437d980dc3db20160ba4dfc135e4e557b8049897
+size 153
