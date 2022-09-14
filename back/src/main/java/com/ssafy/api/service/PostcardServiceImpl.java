125615c05e6735d92dfd7923376d16520afc8571
민경대 <rudwl1005@naver.com>
2022-09-14 13:42:30 +0900

    feat : 검색 API 개발
     - 엽서 태그 검색 추가
     - 재단 이름 검색 추가
    
    
    Former-commit-id: fd4eb64c00bf362fc31858fc8659d719b8fa7b43

--- back/src/main/java/com/ssafy/api/service/PostcardServiceImpl.java
+++ back/src/main/java/com/ssafy/api/service/PostcardServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1ab17697e001025881e87c06ad92ce16422301288a0ac70b0faa012d71285063
-size 6574
+oid sha256:6c9a1199cde4cfaf3bb0d879bbf12c6ff2613aa7d111c60a6195a17f4537c90d
+size 7648
