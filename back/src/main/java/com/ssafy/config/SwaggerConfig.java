69fbd119bfa7612a10b9701034a710de9f312f37
jimin6206 <jimin6206@gmail.com>
2022-09-20 01:55:00 +0900

    test : swagger url test
    swagger url 변경
    
    
    Former-commit-id: 3f8ab6416c075f9e9805aaff926aea12a405923f

--- back/src/main/java/com/ssafy/config/SwaggerConfig.java
+++ back/src/main/java/com/ssafy/config/SwaggerConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c15e5883772b06977f19892d3cb5911c6cc468243fe4d9236f1cc7df2ee8e219
-size 2896
+oid sha256:44835631493f9e52863f6f4ec785da8855dc3aea2b54e984b65fba44fb382d2a
+size 3283
---
+++ back/src/main/java/com/ssafy/config/Workaround.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:339d16d63fce0622ac90d74f27777d046ca99afb508bcc32eaf8e1f87af6f699
+size 1216
