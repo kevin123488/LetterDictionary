0ac9c7d7d35bbbbd61c411868ef8fb30dae5ef5b
박제학 <wpgkr1093@naver.com>
2022-09-23 10:27:25 +0900

    jehak
    Former-commit-id: 874e6b117c06010978037c528a83d609ad89fcc9

--- front/src/views/TestMypageView.vue
+++ front/src/views/TestMypageView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:84008271618e3c6749e214bfbd91e0cde256a9235840028999b6f4d510afda49
-size 18235
+oid sha256:9bf61b368bd5f0500ecc074136c62fa32215a55153ce4ee6eec02d0565bcb609
+size 18232
