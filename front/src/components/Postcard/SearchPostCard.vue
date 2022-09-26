5da58d02bbdea9736c89bf2b2ce079fab3260bc9
박제학 <wpgkr1093@naver.com>
2022-09-26 17:55:26 +0900

    feat: 좋아요로직 디버깅
    Former-commit-id: 8de7131190bfba0a4fa71bf5717113d95a236f9a

--- front/src/components/Postcard/SearchPostCard.vue
+++ front/src/components/Postcard/SearchPostCard.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bd336ddecfcaaf9b5979ccf51d55cb218770574639d2d14d130552926aea24ef
-size 4441
+oid sha256:f69eca8b2f77d2420d3fa38586e6caa7bd25032c04e2e25b2a9d2bc8669a64df
+size 4453
--- front/src/views/TestMypageView.vue
+++ front/src/views/TestMypageView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:69f1ff8ca410e27ed3f45f26925bb8c0e9e383aa785a398e22ae0418717006f5
-size 20143
+oid sha256:9d92e5697ea641bcd9201b622473b927cddda69c2f05b1bdea3967754839cca0
+size 20169
