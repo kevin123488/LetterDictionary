2ffe06198f42b2e700302d239bf1da36d2a8527a
박제학 <wpgkr1093@naver.com>
2022-09-27 10:59:42 +0900

    feat: 재단 상세정보 컴포넌트 변경
    Former-commit-id: 4bd2536ce6e04a3846141ad8f986fc05a37e08fa

--- front/src/views/OrganizationListView.vue
+++ front/src/views/OrganizationListView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5847a3855d33233803b49e0e76237e0d95d804e8cbc5d3a12632f7235da4bc66
-size 7909
+oid sha256:ed500fcf395134dbca2e639758c53b03fd040f1ad5bdc71e47c75b5f20a7bceb
+size 9529
