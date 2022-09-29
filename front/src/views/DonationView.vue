c9978e742536817c54e4cec7985d25438e13d8ff
전현우 <soshot22@naver.com>
2022-09-29 15:54:12 +0900

    fix: 기부 내  마이페이지 이동 로직 기부 내 마이페이지 이동 로직
    Former-commit-id: 2add82999bfac656a61ada1514c57914cb094059

--- front/src/views/DonationView.vue
+++ front/src/views/DonationView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d9b6485b14a2ed08ab66407ff5392f189b757e19cd113a80cebd538d21bc78cf
-size 20546
+oid sha256:c8df7504690933cebfa7704d62a12c2ef2f8ab9d415c7b0c42f86c3720f3bfd5
+size 20640
