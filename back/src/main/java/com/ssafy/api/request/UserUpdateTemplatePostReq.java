8a2f299cb934af2f725ee5d008d2421007dbf7aa
박제학 <wpgkr1093@naver.com>
2022-09-29 18:29:28 +0900

    feat: 마이페이지 기능작업중
    Former-commit-id: 7b120068539ea4bf7609c337ec03250a370f945a

--- back/src/main/java/com/ssafy/api/request/UserUpdateTemplatePostReq.java
+++ back/src/main/java/com/ssafy/api/request/UserUpdateTemplatePostReq.java
@@ -1,3 +1,14 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:695b1518a85e21b0299cbc99584a0bab62a2715a16bb0d31664f0722acfc9b1c
-size 345
+package com.ssafy.api.request;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@ApiModel("UserUpdateTemplatePostRequest")
+public class UserUpdateTemplatePostReq {
+    @ApiModelProperty(name="userTemplate", example="0")
+    int userTemplate;
+}
--- front/src/components/Nav/SideBar.vue
+++ front/src/components/Nav/SideBar.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d429bb609d9e6299c5186c1d4135045de241da078a02ec2c9d4af0bafed3253b
-size 2745
+oid sha256:082bc7f2761b96aa5bb15de7883afe11848168c6a0b1014d59169f344a8d8b9f
+size 2780
