b1bf34b4b61b91b31af5fe35ddf6c7976262e153
전현우 <soshot22@naver.com>
2022-10-03 01:06:20 +0900

    feat: add icon
    change icons
    
    
    Former-commit-id: 7b9b415f2c5f609c3a289c4c7a74322a9e99cdec

--- front/src/views/SearchView.vue
+++ front/src/views/SearchView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:27384188719909d0811effbe954fa01b9c37e36e5c66b27a7f18d9bdb3f1c461
-size 6327
+oid sha256:ac8ac07179e911b5176376034bd8fca33eccf3d94410ba491401217bed734309
+size 6331
