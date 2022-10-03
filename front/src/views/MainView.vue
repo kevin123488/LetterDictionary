6e156393108d6528bc2a2fa9bd0ca02d0345f775
박제학 <wpgkr1093@naver.com>
2022-10-03 13:57:08 +0900

    feat: 마이페이지 팔로우추가
    Former-commit-id: 0dd2b87294bae0c7f8fab87df3e9fcce4fdba618

--- front/src/views/MainView.vue
+++ front/src/views/MainView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:78cceae3b04b8dc7cc1cb3534bba5579df8a4727b36cbc16d4c54b91e3d344e7
-size 28188
+oid sha256:1225b6e09ec87e7f98f98551618dd12f3defe4186e67b6e9cc2ffcf8cc3958e8
+size 28178
