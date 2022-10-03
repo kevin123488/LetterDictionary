f542ec46289792bca802a3c459720a74b4c28a56
전현우 <soshot22@naver.com>
2022-10-03 21:51:36 +0900

    feat: donation template
    change the donation template################
    
    
    Former-commit-id: c471740879fe4044cbd7dda116719584e68343bf

--- front/src/views/DonationView.vue
+++ front/src/views/DonationView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e99fa40bc63a04d8f56db3e1e1155ccd907492855c87f5a803fd4dbba6e7e926
-size 21604
+oid sha256:cc4cb90b49bf53a97a72cbb0ff4e252cfd5fffe166d3d1ecfe9ab8c691ae0568
+size 23320
