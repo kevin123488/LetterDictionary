0875df79d7ef5582bf694efd4c3189e662a3638f
박제학 <wpgkr1093@naver.com>
2022-10-06 06:46:34 +0900

    기부페이지 수정
    Former-commit-id: 64005704e4c8bdcb8e348e57caa35a943e569807 [formerly d326e714c8ccb175590cad932fe043456a18f0ba]
    Former-commit-id: b3d351818f6b522909c8710eb7e68917b2c58d40

--- front/src/views/DonationView.vue
+++ front/src/views/DonationView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dada8263d8bb85d791c7eb0f1d82d56a771ccd7523e4a3bf70469f0c58037b6b
-size 23716
+oid sha256:57bc952e01f18a6599cd20d3ca717e2d098ba10ff6a6d3f70b382a6c938f6f50
+size 24687
