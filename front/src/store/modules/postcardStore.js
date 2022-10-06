306fb91697d7743ec2c3a1be2bf1d28f8d6e90e4
박제학 <wpgkr1093@naver.com>
2022-10-06 11:51:57 +0900

    temp
    Former-commit-id: 1ac1730e68645d6c7a921d115f9415343bcdae34 [formerly 45f03ead9280cb788b6330665e42dcf0fe1411f0]
    Former-commit-id: b07fdcb6eb970e3758a7778e0433eb0dcc5a2251

--- front/src/store/modules/postcardStore.js
+++ front/src/store/modules/postcardStore.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:497a0a7356c92e8be90ce00f2d17afe0ce087dd619057c0e198a7a2fe4d48aad
-size 7627
+oid sha256:127be6c34470326d1c2354e8ffebecfb70c5da49f33a5981ce3c920d53e8b191
+size 7636
