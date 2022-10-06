19bad57433ee58efc4322bcec8fb0e81a4955629
박정현 <kevin123488@naver.com>
2022-10-06 10:17:23 +0900

    feat : front modify
    Former-commit-id: 0caeb22b6c2c0c9ec83e0d7e4cd3b15f68f6b8a1 [formerly cf364b93c7935c831302a3e5903af5076364f54a]
    Former-commit-id: 3a6486a2536671fc19e135e0fa43f395e66ee720

--- front/src/views/HtmlCanvasView.vue
+++ front/src/views/HtmlCanvasView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e0fa881e3a86176edc76e999acd528ef62716e57f3360c5684a671f65a709c49
-size 42307
+oid sha256:53e76d03cc50884327cf93adcec31716042b77fd9a7610592210b7cea1949af8
+size 42334
