299fc3ede2498b0af1f655e4085849e4825050f4
민경대 <rudwl1005@naver.com>
2022-10-06 22:40:40 +0900

    fix : 인공지능 서버 오타해결
    Former-commit-id: 1e5285848529b6b0316bdab1a3112b7d9df0b943 [formerly ec7bb386f68fd934e769a3f2ac004a31c431311f]
    Former-commit-id: a7f95a0597475ea859742bfc97d8941942ba61ac

--- ai/yeopseo/yeopseo/neural_style_preserve_color4.py
+++ ai/yeopseo/yeopseo/neural_style_preserve_color4.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ad48383337a225b63cfb39bf13dbf2f07953293c63aa745a359fbbcfd48a1e87
-size 11385
+oid sha256:cacf640579c28f2b7155fb5d3d41c74e76bdeaeb7e3678893a99b22512f5dc4b
+size 11386
