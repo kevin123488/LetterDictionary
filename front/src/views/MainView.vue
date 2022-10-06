db9a56306928b1e8508fc893761301409038b908
박제학 <wpgkr1093@naver.com>
2022-10-06 14:27:26 +0900

    재단인포 수정
    Former-commit-id: 4924bb6664d1208fc710c46e3bcd695ea75c4ceb [formerly a92bb40f1a95d001af093c6093d3df0bbe1fb892]
    Former-commit-id: 1919c5298d96a331c179c2e1154e26b7cfc0cbb2

--- front/src/views/MainView.vue
+++ front/src/views/MainView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:188b07157b9f1f96a6fe5e3e64dd82d37f240cbd0114c139df053c254e6beb8d
-size 35832
+oid sha256:522a6300dbab156562d91142f8b42cfdca10445129b96150bed56872793a6f72
+size 35928
