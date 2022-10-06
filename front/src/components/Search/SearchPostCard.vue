0975771fcce8da8c8819dc68b6f0e68bef65c32c
박정현 <kevin123488@naver.com>
2022-10-07 00:24:52 +0900

    feat : search modify
    Former-commit-id: bab8849c52fdf573e13bd55f9a56309480bb60b8 [formerly 11b5bcebd72c9deff3e3d2fe2e719d3aa9ff21f7]
    Former-commit-id: e86931b56c41c648c3d46959b6b369eeb358509b

--- front/src/components/Search/SearchPostCard.vue
+++ front/src/components/Search/SearchPostCard.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5d154ee3fa5771ad8163a38fe74a684518bda9fddd186038f92127e47848d6ca
-size 6186
+oid sha256:a6783a0b8f843c8b562db80607b1a736380df4cafbdecd14b169b5d7f8c79637
+size 6337
